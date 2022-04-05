42195730daadea0cb6a2b7360a8c57d9c92c1f3e
Denis Lisunov <rev.denisas@gmail.com>
2022-04-05 23:27:04 +0300

    use rake db:seed to repopulate database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,53 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'open-uri'
+
+#omniva data
+puts "loading json data..."
+data = URI.open('https://www.omniva.lt/locations.json')
+data = JSON.load(data)
+puts "complete!"
+
+#clean up
+puts "deleting previous data..."
+Machine.delete_all()
+puts "complete!"
+
+#populate db
+puts "Repopulating database..."
+data.each_with_index do |item, index|
+  puts "#{index}: adding #{item["NAME"]}"
+  thing = Machine.new(
+    ZIP: item["ZIP"], 
+    NAME: item["NAME"], 
+    TYPE: item["TYPE"], 
+    A0_NAME: item["A0_NAME"], 
+    A1_NAME: item["A1_NAME"], 
+    A2_NAME: item["A2_NAME"], 
+    A3_NAME: item["A3_NAME"], 
+    A4_NAME: item["A4_NAME"], 
+    A5_NAME: item["A5_NAME"], 
+    A6_NAME: item["A6_NAME"], 
+    A7_NAME: item["A7_NAME"], 
+    A8_NAME: item["A8_NAME"], 
+    X_COORDINATE: item["X_COORDINATE"], 
+    Y_COORDINATE: item["Y_COORDINATE"], 
+    SERVICE_HOURS: item["SERVICE_HOURS"], 
+    TEMP_SERVICE_HOURS: item["TEMP_SERVICE_HOURS"], 
+    TEMP_SERVICE_HOURS_UNTIL: item["TEMP_SERVICE_HOURS_UNTIL"], 
+    TEMP_SERVICE_HOURS_2: item["TEMP_SERVICE_HOURS_2"], 
+    TEMP_SERVICE_HOURS_2_UNTIL: item["TEMP_SERVICE_HOURS_2_UNTIL"], 
+    comment_est: item["comment_est"], 
+    comment_eng: item["comment_eng"], 
+    comment_rus: item["comment_rus"], 
+    comment_lav: item["comment_lav"], 
+    comment_lit: item["comment_lit"], 
+    MODIFIED: item["MODIFIED"]
+  )
+
+  thing.save
+
+end
+puts "all done!"
+puts "NOTE: id of first item is #{Machine.first.id} and last item is #{Machine.last.id}"
