1e38228463ae084a721b8448325a2aceb22c23bc
Denis Lisunov <rev.denisas@gmail.com>
2022-04-06 23:24:11 +0300

    added redirect from root for rails routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root "machines#index"
-  resources :machines, only: [:show]
+  root :to => redirect('/machines')
+  resources :machines, only: [:show, :index]
 end
