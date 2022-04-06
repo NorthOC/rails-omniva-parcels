3a23ec0945499f05e4e9574dd7300207e3e4014a
Denis Lisunov <rev.denisas@gmail.com>
2022-04-06 23:28:22 +0300

    added sql filters to index for sorting machines

--- app/controllers/machines_controller.rb
+++ app/controllers/machines_controller.rb
@@ -1,6 +1,23 @@
 class MachinesController < ApplicationController
+  
   def index
-    @machines = Machine.all()
+    if params[:filter] == 'zip'
+      @machines = Machine.where(ZIP: params[:query])
+
+    elsif params[:filter] == 'name'
+      query = params[:query].gsub("+", " ")
+      @machines = Machine.where("lower(NAME) LIKE lower(?)", "%#{query}%")
+      
+    elsif params[:filter] == 'address'
+      query = params[:query]
+      query.gsub!("+", " ")
+      query.gsub!("%2C+", ", ")
+      query.gsub!("%2C" ", ")
+      @machines = Machine.where("lower(FULL_ADDRESS) LIKE lower(?)", "%#{query}%")
+    else
+      @machines = Machine.all()
+    end
+
   end
 
   def show
