7c1b3e32fe875e0052023370c70af18f7590f071
Denis Lisunov <rev.denisas@gmail.com>
2022-04-08 17:07:18 +0300

    added csv exporting function for specific table parameters

--- app/models/machine.rb
+++ app/models/machine.rb
@@ -1,2 +1,15 @@
 class Machine < ApplicationRecord
+
+  #csv
+  def self.to_csv
+    CSV.generate() do |csv|
+      name = %w(ZIP Name Adress)
+      csv << name
+      all.each do |item|
+        row = [item.ZIP, item.NAME, item.FULL_ADDRESS]
+        csv << row
+      end
+    end
+  end
+
 end
